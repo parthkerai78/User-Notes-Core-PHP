1d20d085d46f461a71999c4acbfa8f19f87d62bf
parthkerai78 <parthkerai78@gmail.com>
2026-03-07 13:12:30 +0530

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,16 +1,20 @@
 <?php
-    class Database{
-        private $host = "localhost";
-        private $dbname = "smart_notes";
-        private $username = "root";
-        private $password = "";
-        public $conn;
 
-        public function connect(){
-            $this->conn = new PDO("mysql:host = $this->host; dbname = $this->dbname", $this->username, $this->password);            
-            return $this->conn;
-        }
-        
+class Database {
+    private $host = "localhost";
+    private $db_name = "smart_notes";
+    private $username = "root";
+    private $password = "";
+    public $conn;
+
+    public function connect() {
+        $this->conn = new PDO(
+            "mysql:host=" . $this->host . ";dbname=" . $this->db_name,
+            $this->username,
+            $this->password
+        );
+
+        return $this->conn;
     }
-
+}
 ?>
